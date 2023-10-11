0d9fa38003157f334b3a2bed9d2c58fe928a2db9
bmacharia <babu.macharia@gmail.com>
2023-10-11 19:08:54 +0000

    fixed syntax errors

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     terratowns = {
-      source = "local.providers/local/terratowns"
+      source  = "local.providers/local/terratowns"
       version = "1.0.0"
     }
   }
@@ -23,48 +23,49 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = var.terratowns_endpoint
-  user_uuid= var.teacherseat_user_uuid
-  token=var.terratowns_access_token
+  endpoint  = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token     = var.terratowns_access_token
 }
 
 module "home_chowder_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.chowder.public_path
+  source          = "./modules/terrahome_aws"
+  user_uuid       = var.teacherseat_user_uuid
+  public_path     = var.chowder.public_path
   content_version = var.chowder.content_version
+}
 
 resource "terratowns_home" "home" {
-  name = "Clam Chowder and Sourdough Bread, A Match Made in Heaven"
-  description = <<DESCRIPTION
+  name            = "Clam Chowder and Sourdough Bread, A Match Made in Heaven"
+  description     = <<DESCRIPTION
 East Coast meets West Coast. 
 The marriage of New England Clam chowder with the San Francisco Sourdough Bread 
 is a relatively modern culinary innovation. It's a combination
 that is both practical(edible bowl) and delicious
 DESCRIPTION
-  domain_name = module.home_chowder_hosting.domain_name
-  town = "cooker-cove"
+  domain_name     = module.home_chowder_hosting.domain_name
+  town            = "cooker-cove"
   content_version = var.chowder.content_version
 }
 
 module "home_fender_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.fender.public_path  
+  source          = "./modules/terrahome_aws"
+  user_uuid       = var.teacherseat_user_uuid
+  public_path     = var.fender.public_path
   content_version = var.fender.content_version
 }
 
 resource "terratowns_home" "home_fender" {
- name = "Fender Precision Bass"
- description = <<DESCRIPTION
+  name            = "Fender Precision Bass"
+  description     = <<DESCRIPTION
  Originally designed over 70 years ago, by Leo Fender, The Fender
  Precision Bass or P-Bass has been used to provide the low-end for countless
  hit songs in all genres of music, from Rock and Roll, Motown, Country,
  and any other genre one can think of.
 DESCRIPTION
-  domain_name = module.home_fender_hosting.domain_name
-  town = "melomaniac-mansion"
+  domain_name     = module.home_fender_hosting.domain_name
+  town            = "melomaniac-mansion"
   content_version = var.fender.content_version
-
+}
 
 
